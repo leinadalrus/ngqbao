eba235a3900f5224aecd5d26d3aa07a082e6383d
leinadalrus <daniel.david.surla@gmail.com>
2025-03-13 09:43:29 +0800

    Replaced user-login file(s) location. Fixing routing to have static-site generation.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { provideRouter, RouterModule, Routes, withComponentInputBinding } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { provideHttpClient, withFetch, withJsonpSupport } from '@angular/common/http';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', title: 'Dashboard', component: DashboardComponent },
+  {
+    path: 'DashboardComponent',
+    title: 'Dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'ProductsComponent',
+    title: 'Products',
+    component: ProductsComponent,
+  },
+  { path: '**', title: 'Page Not Found', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    provideHttpClient(withFetch(), withJsonpSupport()),
+    provideRouter(routes, withComponentInputBinding()),
+  ],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,34 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
+// Products
 import { ProductsComponent } from './pages/products/products.component';
+import { EditProductComponent } from './pages/products/edit-product/edit-product.component';
+
 import { MemberLayoutComponent } from './layouts/member-layout/member-layout.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 import { TradingCardComponent } from './components/trading-card/trading-card.component';
-import {
-  provideRouter,
-  Routes,
-  withComponentInputBinding,
-} from '@angular/router';
-import { provideHttpClient, withFetch, withJsonpSupport } from '@angular/common/http';
-import { UserLoginComponent } from './user-login/user-login.component';
-import { EditProductComponent } from './pages/products/edit-product/edit-product.component';
-
-const appRoutes: Routes = [
-  { path: '', title: 'Dashboard', component: DashboardComponent },
-  {
-    path: 'DashboardComponent',
-    title: 'Dashboard',
-    component: DashboardComponent,
-  },
-  {
-    path: 'ProductsComponent',
-    title: 'Products',
-    component: ProductsComponent,
-  },
-  { path: '**', title: 'Page Not Found', component: PageNotFoundComponent },
-];
+import { UserLoginComponent } from './pages/user-login/user-login.component';
 
 @NgModule({
   declarations: [
@@ -48,10 +30,6 @@ const appRoutes: Routes = [
     EditProductComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [
-    provideHttpClient(withFetch(), withJsonpSupport()),
-    provideRouter(appRoutes, withComponentInputBinding()),
-  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
